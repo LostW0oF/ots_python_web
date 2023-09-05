f05997560c016ee8c7e0938d152aa89800cadfa5
Sully Brito <sullyz.sb@gmail.com>
2023-09-05 00:03:45 +0000

    Pagina Basica HTML com flask

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask('meu app')
 
-@app.route('/')
-def hello():
-    return 'IMPOSSIVEL COM ESSA GALERA'
+posts = [
+    {
+        'titulo': 'Minha primeira postagem',
+        'texto': 'teste'
+    },
+    {
+        'titulo': 'Segundo post',
+        'texto': 'teste2'    
+    }
+]
 
-@app.route('/novo')
-def nova():
-    return '<h1> PAGINA NOVA </h1>'
+@app.route('/')
+def exibir_entradas():
+    entradas = posts # Mock das postagens
+    return render_template('exibir_entrada.html', entradas = entradas)
