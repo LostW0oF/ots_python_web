abfdd041b4bcb7bae2da039ffded50838209a0f5
Sully Brito <sullyz.sb@gmail.com>
2023-09-07 00:02:56 +0000

    att layout e criação do login

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request, session, flash, redirect, url_for
 
-app = Flask('meu app')
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'pupum'
 
 posts = [
     {
@@ -22,3 +22,14 @@ def exibir_entradas():
 @app.route('/layout')
 def layout():
     return render_template('layout.html')
+
+@app.route('/login', methods = ["GET", "POST"])
+def login():
+    erro = None
+    if request.method == "POST":
+        if request.form['username'] == 'admin' and request.form['password'] == 'admin':
+            session['logado'] = True
+            flash('Bem vindo!')
+            return redirect(url_for('exibir_entradas'))
+        erro = 'Usuario ou senha Invalidos'
+    return render_template('login.html', erro = erro)
